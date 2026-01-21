ed4374e5fafb89bb7250af81d655ec50cda2a192
Mehmet Can Karabulut <mehmetcan.karabulut96@gmail.com>
2026-01-21 17:56:36 +0300

    feat: added new repository methods

--- src/main/java/org/example/RentACar/dataAccess/abstracts/ModelRepository.java
+++ src/main/java/org/example/RentACar/dataAccess/abstracts/ModelRepository.java
@@ -3,10 +3,15 @@ package org.example.RentACar.dataAccess.abstracts;
 import org.example.RentACar.entities.concretes.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ModelRepository extends JpaRepository<Model, Integer> {
     boolean existsByBrandId(int brandId);
     boolean existsById(int id);
     Optional<Model> findByNameIgnoreCase(String name);
+    boolean existsByName(String name);
+
+    int countByBrandId(int brandId);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, int id);
 }
