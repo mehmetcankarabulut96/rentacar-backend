c0a45dd20a35d7588c57bcf03d7a44a666b9e800
Mehmet Can Karabulut <mehmetcan.karabulut96@gmail.com>
2026-02-04 09:59:32 +0300

    feat(model): added unit test for update service method

--- src/main/java/org/example/RentACar/business/requests/Model/UpdateModelRequest.java
+++ src/main/java/org/example/RentACar/business/requests/Model/UpdateModelRequest.java
@@ -4,9 +4,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateModelRequest {
 
     @NotNull
--- src/test/java/org/example/RentACar/business/concretes/ModelManagerTest.java
+++ src/test/java/org/example/RentACar/business/concretes/ModelManagerTest.java
@@ -2,10 +2,13 @@ package org.example.RentACar.business.concretes;
 
 import org.example.RentACar.business.abstracts.BrandService;
 import org.example.RentACar.business.requests.Model.CreateModelRequest;
+import org.example.RentACar.business.requests.Model.UpdateModelRequest;
 import org.example.RentACar.business.rules.ModelBusinessRules;
 import org.example.RentACar.dataAccess.abstracts.ModelRepository;
+import org.example.RentACar.entities.concretes.Brand;
 import org.example.RentACar.entities.concretes.Model;
 import org.example.RentACar.utils.exception.BusinessException;
+import org.example.RentACar.utils.exception.Model.ModelNotFoundException;
 import org.example.RentACar.utils.mapper.Model.ModelMapperService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +16,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.swing.text.html.Option;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -94,5 +102,87 @@ public class ModelManagerTest {
         assertThrows(BusinessException.class, () -> modelManager.add(request));
 
         verifyNoInteractions(modelMapperService);
+    }
+
+    @Test
+    void update_whenBusinessRulesPass_shouldUpdateAndSaveModel(){
+
+        UpdateModelRequest request = new UpdateModelRequest(1, "Benz_v2", 1);
+
+        Brand currentBrand = new Brand();
+        currentBrand.setId(2);
+
+        Model currentModel = new Model();
+        currentModel.setId(1);
+        currentModel.setName("Benz");
+        currentModel.setBrand(currentBrand);
+
+        when(modelRepository.findById(1)).thenReturn(Optional.of(currentModel));
+
+        modelManager.update(request);
+
+        verify(modelBusinessRules).checkIfModelNameExistsForUpdate(1, "Benz_v2");
+        verify(modelBusinessRules).checkIfBrandCanBeChanged(1, 2);
+        verify(modelMapperService).updateModel(request, currentModel);
+        verify(modelRepository).save(currentModel);
+    }
+
+    @Test
+    void update_whenModelDoesNotExist_shouldThrowException(){
+
+        UpdateModelRequest request = new UpdateModelRequest(1, "Benz_v2", 1);
+
+        when(modelRepository.findById(1))
+                .thenReturn(Optional.empty());
+
+        assertThrows(ModelNotFoundException.class, () -> modelManager.update(request));
+
+        verifyNoInteractions(modelBusinessRules);
+    }
+
+    @Test
+    void update_whenModelAlreadyExist_shouldThrowException(){
+
+        UpdateModelRequest request = new UpdateModelRequest(1, "Benz", 1);
+
+        Model currentModel = new Model();
+
+        when(modelRepository.findById(1)).thenReturn(Optional.of(currentModel));
+
+        doThrow(new BusinessException("Model Benz is already exists"))
+                .when(modelBusinessRules).checkIfModelNameExistsForUpdate(1, "Benz");
+
+        assertThrows(BusinessException.class, () -> modelManager.update(request));
+
+        verify(modelRepository).findById(1);
+        verify(modelBusinessRules).checkIfModelNameExistsForUpdate(1, "Benz");
+        verifyNoMoreInteractions(modelBusinessRules);
+    }
+
+    @Test
+    void update_whenBrandCantBeChanged_shouldThrowException(){
+
+        UpdateModelRequest request = new UpdateModelRequest(1, "Benz", 1);
+
+        Brand currentBrand = new Brand();
+        currentBrand.setId(2);
+        currentBrand.setName("Mercedes");
+
+        Model currentModel = new Model();
+        currentModel.setId(1);
+        currentModel.setName("Benz");
+        currentModel.setBrand(currentBrand);
+
+        when(modelRepository.findById(1)).thenReturn(Optional.of(currentModel));
+
+        doThrow(new BusinessException("brand can not be changed"))
+                .when(modelBusinessRules)
+                .checkIfBrandCanBeChanged(1, 2);
+
+        assertThrows(BusinessException.class, () -> modelManager.update(request));
+
+        verify(modelBusinessRules).checkIfModelNameExistsForUpdate(1, "Benz");
+        verifyNoInteractions(modelMapperService);
+        verifyNoInteractions(modelRepository);
     }
 }
